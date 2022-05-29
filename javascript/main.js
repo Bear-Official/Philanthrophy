3f7da5118f2261a09e48c05969deed802516cef8
Bear <danekgdgdg@gmail.com>
2022-05-30 01:20:29 +0300

    Added nav bar menu for mobile

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+// var menuButton = $(".nav--btn");
+// menuButton.on("click", function () {
+//     console.log("Click on button");
+//     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+// });
+ 
+
