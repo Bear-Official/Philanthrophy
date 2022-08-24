72b9d9e8c2f7b85dc05480c8adf312eda58c9d68
Bear <danekgdgdg@gmail.com>
2022-08-24 22:15:18 +0300

    [Added] Gulp & fixed bugs

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,16 @@
-// let btn = document.querySelector(".btnn");
-// let clip = document.querySelector(".clip");
-// let close = document.querySelector(".close");
-// let video = document.querySelector(".video");
-// btn.onclick = function () {
-//     btn.classList.add("active");
-//     clip.classList.add("active");
-//     video.play();
-// };
-// close.onclick = function () {
-//     btn.classList.remove("active");
-//     clip.classList.remove("active");
-//     video.pause();
-// };
+let btn = document.querySelector(".video__btn");
+let clip = document.querySelector(".clip");
+let close = document.querySelector(".close");
+let video = document.querySelector(".video");
+btn.onclick = function () {
+    btn.classList.add("active");
+    clip.classList.add("active");
+    video.play();
+};
+close.onclick = function () {
+    btn.classList.remove("active");
+    clip.classList.remove("active");
+    video.pause();
+};
 
-// AOS.init();
+AOS.init();
