ba756f004bd0f8dc89fe9a34b151d4cfd6906a52
Bear <danekgdgdg@gmail.com>
2022-09-27 22:48:51 +0300

    [Fixed] Video button

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,14 @@
-let btn = document.querySelector(".video__btn");
+let btn = document.querySelector(".btn-video");
 let clip = document.querySelector(".clip");
 let close = document.querySelector(".close");
 let video = document.querySelector(".video");
+
 btn.onclick = function () {
     btn.classList.add("active");
     clip.classList.add("active");
     video.play();
 };
+
 close.onclick = function () {
     btn.classList.remove("active");
     clip.classList.remove("active");
